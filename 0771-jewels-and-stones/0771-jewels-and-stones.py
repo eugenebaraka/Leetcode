b5ab3abc2cecf40311f81c2f799cdafa185a7140
Eugene Baraka <97700750+eugenebaraka@users.noreply.github.com>
2023-04-10 10:06:56 -0400

    Time: 28 ms (86.69%), Space: 13.9 MB (45.26%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,4 +1,7 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        return len([s for s in stones if s in jewels])
+        # membership testing in set faster than string searching ()(1)
+        jewels_set = set(jewels)
+        
+        return sum(1 for stone in stones if stone in jewels_set)
         
