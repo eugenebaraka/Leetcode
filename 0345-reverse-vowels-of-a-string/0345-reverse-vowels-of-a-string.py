bd54eb143b8dc84b9d7e0d45cca3a048ade6dd6f
Eugene Baraka <97700750+eugenebaraka@users.noreply.github.com>
2023-06-10 18:56:42 -0600

    Time: 88 ms (27.75%), Space: 17.3 MB (75.25%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -2,25 +2,25 @@ class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
         
-        s_list = [char for char in s]
-        i = 0
-        j = len(s_list) - 1
+        s_list = list(s)
+        left = 0
+        right = len(s_list) - 1
         
-        while i < j:
-            if s_list[i] in vowels and s_list[j] in vowels:
-                s_list[i], s_list[j] = s_list[j], s_list[i]
+        while left < right:
+            if s_list[left] in vowels and s_list[right] in vowels:
+                s_list[left], s_list[right] = s_list[right], s_list[left]
                 
-                i += 1
-                j -= 1
+                left += 1
+                right -= 1
                 
-            elif s_list[i] in vowels:
-                j -= 1
+            elif s_list[left] in vowels:
+                right -= 1
                 
-            elif s_list[j] in vowels:
-                i += 1
+            elif s_list[right] in vowels:
+                left += 1
                 
             else:
-                i += 1
-                j -= 1
+                left += 1
+                right -= 1
                 
-        return "".join(char for char in s_list)
+        return "".join(s_list)
